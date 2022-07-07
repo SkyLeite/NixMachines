946eedd9dbfe8c122cb600e5ff407bfc8b31e1ed
Sky <sky@leite.dev>
2022-07-07 13:03:04 -0300

    Add cachix

--- machines/home-server/default.nix
+++ machines/home-server/default.nix
@@ -125,8 +125,14 @@
     package = pkgs.nixUnstable;
 
     settings = {
-      substituters = [ "http://cache.nixos.org" ];
+      substituters =
+        [ "https://nix-community.cachix.org" "http://cache.nixos.org" ];
+
       trusted-substituters = [ "http://cache.nixos.org" ];
+
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
 
     gc.automatic = false;
