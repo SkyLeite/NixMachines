1134a41c9af3850d65d49e1a95cc83be4e88cbe0
Sky <sky@leite.dev>
2022-07-07 10:45:09 -0300

    Remove pagekite

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.home-manager.url = "github:nix-community/home-manager";
-  inputs.pagekite.url = "path:./packages/pagekite";
 
   outputs = { self, nixpkgs, home-manager, ... }@attrs: {
     nixosConfigurations.home-server = nixpkgs.lib.nixosSystem {
@@ -11,7 +10,5 @@
       specialArgs = { inherit attrs; };
       modules = [ ./machines/home-server/default.nix ];
     };
-
-    # packages.x86_64-linux.pagekite = import ./packages/pagekite/flake.nix;
   };
 }
