a14faff4ff99ffc67194b8728f8791fcf03a1775
Sky <sky@leite.dev>
2022-07-20 15:28:44 -0300

    Add allowlist for github IPs for webhooks

--- machines/miles/default.nix
+++ machines/miles/default.nix
@@ -57,6 +57,21 @@ in {
         trigger-rule = {
           and = [
             {
+              or = map (x: {
+                match = {
+                  type = "ip-whitelist";
+                  ip-range = x;
+                };
+              }) [
+                "192.30.252.0/22"
+                "185.199.108.0/22"
+                "140.82.112.0/20"
+                "143.55.64.0/20"
+                "2a0a:a440::/29"
+                "2606:50c0::/32"
+              ];
+            }
+            {
               match = {
                 type = "value";
                 value = "refs/heads/main";
