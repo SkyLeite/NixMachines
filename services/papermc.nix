95e3dd9e663a0f9798b66ea0cc50d004c98031a4
Sky <sky@leite.dev>
2022-07-23 19:44:49 -0300

    Fix workingdirectory

--- services/papermc.nix
+++ services/papermc.nix
@@ -40,8 +40,10 @@ in {
       requires = [ "network-online.target" ];
       serviceConfig = {
         Nice = 1;
-        WorkingDirectory = "/opt/minecraft/server";
-        ExecStart = "${pkgs.papermc}/bin/minecraft-server";
+        StateDirectory = "papermc-server";
+        WorkingDirectory = "/var/lib/papermc-server";
+        ExecStart =
+          "${pkgs.papermc}/bin/minecraft-server -Xms10G -Xmx10G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true";
         TimeoutStartSec = 0;
         TimeoutStopSec = 120;
         Restart = "always";
