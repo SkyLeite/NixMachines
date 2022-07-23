8b29070ae38978f6f9d5749e4663a872fff23633
Sky <sky@leite.dev>
2022-07-22 23:09:41 -0300

    Maybe write generation before service is killed

--- machines/common/build-machine.nix
+++ machines/common/build-machine.nix
@@ -1,12 +1,11 @@
 { writeShellScriptBin, pkgs, stdenv }:
 let
   wrapper = writeShellScriptBin "build-machine" ''
-     ${pkgs.nixos-rebuild}/bin/nixos-rebuild \
-           --flake github:SkyLeite/NixMachines/$1 \
-           --option extra-builtins-file ./extra-builtins.nix \
-           switch;
-
-    nix-env --list-generations --profile /nix/var/nix/profiles/system | tail -n 1 | tee /tmp/current-generation
+    ${pkgs.nixos-rebuild}/bin/nixos-rebuild \
+          --flake github:SkyLeite/NixMachines/$1 \
+          --option extra-builtins-file ./extra-builtins.nix \
+          switch && \
+          nix-env --list-generations --profile /nix/var/nix/profiles/system | tail -n 1 | tee /tmp/current-generation;
   '';
 in stdenv.mkDerivation {
   name = "build-machine";
