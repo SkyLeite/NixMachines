c5c5e1aae9287033df701202df234f89944783ae
Sky <sky@leite.dev>
2022-07-22 22:39:09 -0300

    Write current generation to /root instead of /run

--- machines/common/build-machine.nix
+++ machines/common/build-machine.nix
@@ -6,7 +6,7 @@ let
            --option extra-builtins-file ./extra-builtins.nix \
            switch;
 
-    nix-env --list-generations --profile /nix/var/nix/profiles/system | tail -n 1 | tee /run/current-generation;
+    nix-env --list-generations --profile /nix/var/nix/profiles/system | tail -n 1 | tee /root/current-generation
   '';
 in stdenv.mkDerivation {
   name = "build-machine";
