bdbd4e935cdc4a31aa5d4015ce3d4f8d7f0126f9
Sky Leite <sky@leite.dev>
2022-12-22 11:42:06 -0300

    Use VM instead of docker container for HASS

--- services/home-assistant.nix
+++ services/home-assistant.nix
@@ -24,31 +24,57 @@ in {
       port = 8123;
     };
 
-    systemd.services.home-assistant = {
-      startLimitIntervalSec = 14400;
-      startLimitBurst = 10;
-      wantedBy = [ "multi-user.target" ];
-      after = [ "docker.service" "docker.socket" ];
-      requires = [ "docker.service" "docker.socket" ];
-      preStop = "${pkgs.docker}/bin/docker stop home-assistant";
-      reload = "${pkgs.docker}/bin/docker restart home-assistant";
-      serviceConfig = {
-        ExecStartPre = "-${pkgs.docker}/bin/docker rm -f home-assistant";
-        ExecStopPost = "-${pkgs.docker}/bin/docker rm -f home-assistant";
-        TimeoutStartSec = 0;
-        TimeoutStopSec = 120;
-        Restart = "on-abnormal";
-        ExecStart = ''
-          ${pkgs.docker}/bin/docker run \
-            --name home-assistant \
-            --privileged \
-            --restart=unless-stopped \
-            -e TZ=America/Sao_Paulo \
-            -v /srv/home-assistant/config:/config \
-            --network=host \
-            ghcr.io/home-assistant/home-assistant:stable
-        '';
+    virtualisation = {
+      libvirtd = {
+        enable = true;
+        # Used for UEFI boot of Home Assistant OS guest image
+        qemuOvmf = true;
       };
     };
+
+    environment.systemPackages = with pkgs; [
+      # For virt-install
+      virt-manager
+
+      # For lsusb
+      usbutils
+    ];
+
+    networking.defaultGateway = "10.0.0.1";
+    networking.bridges.br0.interfaces = [ "enp2s0" ];
+    networking.interfaces.br0 = {
+      useDHCP = false;
+      ipv4.addresses = [{
+        "address" = "10.0.0.5";
+        "prefixLength" = 24;
+      }];
+    };
+
+    # systemd.services.home-assistant = {
+    #   startLimitIntervalSec = 14400;
+    #   startLimitBurst = 10;
+    #   wantedBy = [ "multi-user.target" ];
+    #   after = [ "docker.service" "docker.socket" ];
+    #   requires = [ "docker.service" "docker.socket" ];
+    #   preStop = "${pkgs.docker}/bin/docker stop home-assistant";
+    #   reload = "${pkgs.docker}/bin/docker restart home-assistant";
+    #   serviceConfig = {
+    #     ExecStartPre = "-${pkgs.docker}/bin/docker rm -f home-assistant";
+    #     ExecStopPost = "-${pkgs.docker}/bin/docker rm -f home-assistant";
+    #     TimeoutStartSec = 0;
+    #     TimeoutStopSec = 120;
+    #     Restart = "on-abnormal";
+    #     ExecStart = ''
+    #       ${pkgs.docker}/bin/docker run \
+    #         --name home-assistant \
+    #         --privileged \
+    #         --restart=unless-stopped \
+    #         -e TZ=America/Sao_Paulo \
+    #         -v /srv/home-assistant/config:/config \
+    #         --network=host \
+    #         ghcr.io/home-assistant/home-assistant:stable
+    #     '';
+    #   };
+    # };
   };
 }
