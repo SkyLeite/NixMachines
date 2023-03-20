813aaf0562e66df35df8dcc85bad43b16d4b2294
Sky Leite <sky@leite.dev>
2023-03-20 17:03:05 -0300

    Add WIP hydra eww window

---
+++ users/sky/eww/hydra.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+DELIMITER="|"
+
+declare -A HYDRA_MAIN
+HYDRA_MAIN["q"]="eww -c /mnt/hdd/projects/nix/users/sky/eww close-all${DELIMITER}quit"
+HYDRA_MAIN["r"]="rofi -show run${DELIMITER}Run a program in Rofi"
+HYDRA_MAIN["w"]="rofi -show run${DELIMITER}Switch to a window"
+HYDRA_MAIN["f"]="rofi -show firefox${DELIMITER}Switch to a Firefox window using Rofi"
+HYDRA_MAIN["p"]="pavucontrol${DELIMITER}Open volume mixer"
+HYDRA_MAIN["s"]="flameshot gui${DELIMITER}Take a screenshot"
+HYDRA_MAIN["e"]="rofi -show emoji${DELIMITER}Open emoji picker"
+
+exec_hydra_command() {
+    hydra_name=$1
+    key=$2
+
+    declare -n hydra=$hydra_name
+
+    entry=$(hydra_entry_to_json "$key" "${hydra[$command]}")
+    command=$(jq -r '.command' <<<"$entry")
+    eval "$command"
+}
+
+hydra_entry_to_json() {
+    key=$1
+    hydra_entry=$2
+
+    command=$(cut -f1 -d$DELIMITER <<<"$hydra_entry")
+    description=$(cut -f2 -d$DELIMITER <<<"$hydra_entry")
+
+    jq \
+        -n \
+        --arg key "$key" \
+        --arg command "$command" \
+        --arg description "$description" \
+        '{ "key": $key, "command": $command, "description": $description }'
+}
+
+kill_hydra() {
+    eww -c . close hydra
+}
+
+case "$1" in
+    "exec")
+        hydra_name="$2"
+        command="$3"
+
+        declare -n hydra=$hydra_name
+
+        exec_hydra_command "$hydra_name" "$command" &
+        kill_hydra
+        ;;
+
+    "list")
+        hydra_name="$2"
+        declare -n hydra=$hydra_name
+
+        json_output="[]"
+
+        for key in "${!hydra[@]}"; do
+            value="${hydra[$key]}"
+            command=$(cut -f1 -d$DELIMITER <<<"$value")
+            description=$(cut -f2 -d$DELIMITER <<<"$value")
+
+            entry_json=$(hydra_entry_to_json "$key" "$value")
+
+            json_output=$(
+                jq \
+                    --argjson entry "$entry_json" \
+                    '. += [$entry]' <<<"$json_output"
+            )
+        done
+
+        echo "$json_output"
+        eww -c . update hydra-options="$json_output"
+        ;;
+esac
