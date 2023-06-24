8050a54abd6804764d0c4d029e050c46b9e3ad5d
Sky <sky@leite.dev>
2023-06-24 18:12:30 -0300

    Allow users to reboot

--- machines/home/default.nix
+++ machines/home/default.nix
@@ -119,6 +119,7 @@ in {
     videoDrivers = [ "amdgpu" ];
   };
 
+  services.ddccontrol.enable = true;
   services.ratbagd.enable = true;
 
   services.greetd = {
@@ -331,6 +332,25 @@ in {
           '';
         };
       };
+    };
+
+    polkit = {
+      extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          if (
+            subject.isInGroup("users")
+              && (
+                action.id == "org.freedesktop.login1.reboot" ||
+                action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+                action.id == "org.freedesktop.login1.power-off" ||
+                action.id == "org.freedesktop.login1.power-off-multiple-sessions" ||
+              )
+            )
+          {
+            return polkit.Result.YES;
+          }
+        })
+      '';
     };
   };
 
