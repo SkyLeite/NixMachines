c566cb92b1f54318dcad65d3feb6e4e2b9af5582
Sky <sky@leite.dev>
2023-07-05 19:58:30 -0300

    Server config stuff

--- machines/miles/srb2kart/default.nix
+++ machines/miles/srb2kart/default.nix
@@ -13,8 +13,23 @@ in {
         http_source "kart.zerolab.app"
         advertise On
         karteliminatelast No
+        kartspeed 2
         servername "Dr. Zap - discord.gg/invite/r-br"
         server_contact "sky@leite.dev"
+
+        hm_specbomb On
+        hm_specbomb_antisoftlock On
+        hm_timelimit 5
+        hm_bail On
+        hm_motd On
+        hm_motd_nag Off
+
+        hm_motd_name "Dr. Zap"
+        hm_motd_contact "\134discord.gg/invite/r-br"
+        hm_motd_tagline "Suco de múmia"
+
+        hm_restat On
+        hm_restat_notify On
       '';
     };
   };
