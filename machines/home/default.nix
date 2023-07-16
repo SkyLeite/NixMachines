45555b9e1ff63196c73d2f9135dc31b9e934bcd5
Sky <sky@leite.dev>
2023-07-16 14:44:44 -0300

    Fix hyprland QT_QPA_PLATFORM

--- machines/home/default.nix
+++ machines/home/default.nix
@@ -23,7 +23,7 @@ let
   hyprlandLauncher = pkgs.writeShellScript "hyprlandLauncher" ''
     export SDL_VIDEODRIVER=wayland
     export _JAVA_AWT_WM_NONREPARENTING=1
-    export QT_QPA_PLATFORM=hyprland
+    export QT_QPA_PLATFORM=wayland
     export XDG_CURRENT_DESKTOP=hyprland
     export XDG_SESSION_DESKTOP=hyprland
     exec Hyprland $@
