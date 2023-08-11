90a82d85e728bb552318c720e85805f925557513
Sky <sky@leite.dev>
2023-08-11 11:33:31 -0300

    Enable xwayland in hyprland

--- users/sky/hyprland/default.nix
+++ users/sky/hyprland/default.nix
@@ -5,6 +5,7 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
+    xwayland.enable = true;
     extraConfig = builtins.replaceStrings [ "@@kb_file@@" ]
       [ (builtins.toString ../us-br.xkb) ] (builtins.readFile ./hyprland.conf);
   };
