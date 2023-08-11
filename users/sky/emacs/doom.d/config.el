d828e03ab2cc0459f2c3eed97f582f5a6007db68
Sky <sky@leite.dev>
2023-08-11 11:33:20 -0300

    Change emacs styling

--- users/sky/emacs/doom.d/config.el
+++ users/sky/emacs/doom.d/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 15)
+     doom-variable-pitch-font (font-spec :family "Comic Sans MS" :size 13)) ;; haha funny
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-homage-white)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -124,3 +124,7 @@
 (after! lsp
   (setq lsp-fsharp-use-dotnet-tool-for-fsac t)
   (setq lsp-clients-lua-language-server-bin "/nix/store/z2f0fiq74nmnqq9gw2xbp9n2b3fvbaz0-user-environment/bin/lua-language-server"))
+
+;; (after! format-all-mode
+;;   (advice-add 'format-all-buffer :around #'envrc-propagate-environment)
+;;   (advice-add 'format-all-buffer--from-hook :around #'envrc-propagate-environment))
