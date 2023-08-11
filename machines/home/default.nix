196110337ebd27d6e07f7817bcbb9322b5eca32c
Sky <sky@leite.dev>
2023-08-11 11:34:30 -0300

    Fix Steam missing libraries

--- machines/home/default.nix
+++ machines/home/default.nix
@@ -435,6 +435,21 @@ in {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
+    package = pkgs.steam.override {
+      extraPkgs = pkgs:
+        with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
+    };
   };
   programs.adb.enable = true;
 
